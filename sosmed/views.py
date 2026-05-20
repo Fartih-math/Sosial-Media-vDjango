b1e5d0032b26a43b17a217b51fa0b26e60d89d71
Fartih-math <farisfatihmtk@gmail.com>
2026-05-20 22:25:03 +0700

    Delete sosmed directory

--- sosmed/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import UserPost
-
-def list_akun(request):
-    semua_akun = UserPost.objects.all()
-    return render(request, 'list.html', {'semua_akun': semua_akun})
